e217fb0cdc8464874011c4ad5075c299409af63e
laplandalone <sunnyrhq@163.com>
2016-01-16 18:25:31 +0800

    修复bug

--- WebRoot/js/refund/refundApprove.js
+++ WebRoot/js/refund/refundApprove.js
@@ -93,7 +93,7 @@ $(document).ready(function() {
 				}
 			});
 			approveObj.remark = approveRemark;
-			if("104" == nextState) {
+			if("104" == nextState || "105" == nextState) {
 				approveObj.masterType = masterType;
 			} else if("108" == nextState) {
 				approveObj.isTransfer = isTransfer;
