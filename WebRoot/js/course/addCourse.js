ecd96ebb936173683d30f02325a67ca53b5c889b
laplandalone <laplandalone@163.com>
2016-04-27 15:26:14 +0800

    缴费时间校验

--- WebRoot/js/course/addCourse.js
+++ WebRoot/js/course/addCourse.js
@@ -31,6 +31,17 @@ function initPayDate()
 //			return d1<=date && date<=d2;
 //		}
 //	});
+	
+		$('#payDate').datebox().datebox('calendar').calendar(
+	{
+		validator: function(date)
+		{
+			var now = new Date();
+			//var d1 = new Date(now.getFullYear(), now.getMonth(), now.getDate()-30);
+			var d2 = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+			return  date<=d2;
+		}
+	});
 } 
 
 
